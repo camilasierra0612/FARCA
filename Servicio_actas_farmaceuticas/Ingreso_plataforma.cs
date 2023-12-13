2aca85819defaa9162e47796e156d0225e56c01d
camilasierra0612 <148457452+camilasierra0612@users.noreply.github.com>
2023-12-12 22:05:01 -0500

    Add files via upload

---
+++ Servicio_actas_farmaceuticas/Ingreso_plataforma.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Servicio_actas_farmaceuticas
+{
+    public partial class Ingreso_plataforma : Form
+    {
+        public Ingreso_plataforma()
+        {
+            InitializeComponent();
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "Camila1" && txtContraseña.Text == "6789" || txtUsuario.Text == "Admin" && txtContraseña.Text == "5678" )
+            {
+                MessageBox.Show("Ha ingresado al sistema con éxito");
+
+                var frm = new Menu_opciones();
+                frm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o Contraseña incorrecta");
+            }
+        }
+
+        private void btnVerContraseña_Click(object sender, EventArgs e)
+        {
+            if (txtContraseña.PasswordChar == (char)0)
+            {
+                txtContraseña.PasswordChar = '*';
+            }
+            else
+            {
+                txtContraseña.PasswordChar = (char)0;
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Servicio_actas_farmaceuticas/Menu_opciones.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Servicio_actas_farmaceuticas
+{
+    public partial class Menu_opciones : Form
+    {
+        public Menu_opciones()
+        {
+            InitializeComponent();
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void btnIngresoActas_Click(object sender, EventArgs e)
+        {
+            var frm = new Ingreso_actas();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnRegistroLaboratorio_Click(object sender, EventArgs e)
+        {
+            var frm = new Registro_laboratorio();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void btnRegistroProducto_Click(object sender, EventArgs e)
+        {
+            var frm = new Registro_producto();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void btnIngresarDatosProveedor_Click(object sender, EventArgs e)
+        {
+            var frm = new Registro_proveedor();
+            frm.Show();
+            this.Hide();
+        }
+    }
+}
